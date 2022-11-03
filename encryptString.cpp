a6eb341248515c066550a0e1d03bfc7539001e14
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-11-03 16:18:17 +0200

    Create encryptString.cpp
    To illustrate a few concepts, create a main function
    that encrypts a simple string.

---
+++ encryptString.cpp
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	char* str = (char*)"http://192.168.1.3:8888/beacon"; // Treat string as an array of characters.
+	int length = strlen(str);
+	char* buf = (char*)malloc(length);
+
+	// your encryption here
+
+	printf("Encrypted String: %s", buf);
+
+}
