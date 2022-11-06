bf17deaae15e37a0fb0d92b0bca07be0133e40c2
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-11-06 12:39:01 +0200

    Create main header file.
    Create main header file and link to main cpp file (encryptString).

--- encryptString.cpp
+++ encryptString.cpp
@@ -1,10 +1,11 @@
-#include <string>
+#include "main.h"
+
 using namespace std;
 
 
 int main()
 {
-	char* str = (char*)"http://192.168.1.3:8888/beacon"; // Treat string as an array of characters.
+	char* str = (char*)"http://192.168.1.3:8888/beacon"; // Treat string as an array of chars.
 	int length = strlen(str);
 	char* buf = (char*)malloc(length);
 
