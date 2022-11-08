2fbf05ab1f22036a88209c4a7b568b7155f506de
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-11-08 15:42:06 +0200

    Create encryptString function.
    Function encrypts a string using XOR.

--- encryptString.cpp
+++ encryptString.cpp
@@ -1,28 +1,28 @@
 #include "Main.h"
+#include <iostream>
 
 using namespace std;
 
+// Function with a simple loop that encrypts a string using XOR
+
+void EncryptString(char* str, int length, char* buf, char key) {
+	for (int i = 0; i < length; i++) {
+		buf[i] = str[i] ^ key;
+	}
+	
+}
 
 int main()
+
 {
 	char* str = (char*)"http://192.168.1.3:8888/beacon"; // Treat string as an array of chars.
 	int length = strlen(str);
 	char* buf = (char*)malloc(length);
 
-	if (buf > 0) {
-
-
-	}
-
-	else if (buf < 0) {
-
-	}
-
-	else {
-
-	}
+	
 	// your encryption here
+	EncryptString(str, length, buf, 0x01);
 
-	printf("Encrypted String: %s", buf);
+	printf("Encrypted String: %s\n", buf);
 
 }
