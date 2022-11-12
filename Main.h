08ecba521ce2b9df1ce5aaf12035f13c0b2d1777
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-11-12 16:43:21 +0200

    Create internet.cpp.
    Add iostream header to main header file,
    remove iostream header from main cpp file.

--- Main.h
+++ Main.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <Windows.h>
 #include <wininet.h>
 #pragma comment (lib, "wininet.dll")
--- redTeamAgent.cpp
+++ redTeamAgent.cpp
@@ -1,5 +1,5 @@
 // A step by step trojan www.grantknoetze.com
-#include <iostream>
+
 #include <Windows.h>
 #include <wininet.h>
 #pragma comment (lib, "wininet.dll")
