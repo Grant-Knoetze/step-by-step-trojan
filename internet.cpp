4dc1218bb7436504e367bd25cda6daa5848aea3b
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-11-13 14:49:53 +0200

    Create internet connect handle.

--- internet.cpp
+++ internet.cpp
@@ -1,8 +1,13 @@
+// This internet.cpp file enables internet functionality for redTeamAgent
 #include "Main.h"
 
 
+/*Initialize the use of the WinInet functions, and provide
+the functionality to request and read files on an HTTP server*/ 
+
 LPCSTR UserAgent = "Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36 (KHTML, like Gecko) Chrome / 107.0.0.0 Safari / 537.36";
 
-
-HINTERNET SendRequest(LPCSTR Domain, int Port, LPCSTR URI, LPCSTR Verb, const char* Params);
-
+HINTERNET SendRequest(LPCSTR Domain, int Port, LPCSTR URI, LPCSTR Verb, const char* Params) {
+	HINTERNET InternetHandle = InternetOpen(UserAgent, INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0 );
+	HINTERNET ConnectHandle = InternetConnect(InternetHandle, Domain, Port, NULL, NULL, INTERNET_SERVICE_HTTP, 0, NULL  ); // Connect to the internet.
+}; // Send request to HTTP server
