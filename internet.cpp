1627fd6c16ba2a0d28704314fe28ddff798af068
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-11-13 14:31:09 +0200

    Add user agent variable to internet.cpp.
    Add chrome user agent as a variable for use in
    HTTPSendRequest function. Necessary as a value.

--- internet.cpp
+++ internet.cpp
@@ -1,4 +1,8 @@
 #include "Main.h"
 
+
+LPCSTR UserAgent = "Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36 (KHTML, like Gecko) Chrome / 107.0.0.0 Safari / 537.36";
+
+
 HINTERNET SendRequest(LPCSTR Domain, int Port, LPCSTR URI, LPCSTR Verb, const char* Params);
 
